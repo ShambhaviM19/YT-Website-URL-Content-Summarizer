4815a6b103de9e2f0ad7052da432eaca195ae059
ShambhaviM19 <122414069+ShambhaviM19@users.noreply.github.com>
2024-09-05 23:12:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
 
+#####
+
 st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
 st.title("🦜 LangChain: Summarize Text From YT or Website")
 st.subheader('Summarize URL')
